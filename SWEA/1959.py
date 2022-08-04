269cc3da4d208b27e05a815806c27f5fb418e2f5
김도언 <vov06144@naver.com>
2022-08-05 00:53:38 +0900

    Add SWEA_1959, Revise SWEA_1204

--- SWEA/1959.py
+++ SWEA/1959.py
@@ -1,1 +1,30 @@
 # 두 개의 숫자열
+
+T = int(input())
+for test_case in range(1, T + 1):
+
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    if len(A) > len(B):
+        L = A[:]
+        S = B[:]
+    else:
+        L = B[:]
+        S = A[:]
+
+    # 위와 동일 - pythonic한 방법
+    # if len(A) < len(B):
+    #     A, B = B, A
+
+    num = []
+
+    for i in range((len(L) - len(S) + 1)):
+        sum = 0
+        for j in range(len(S)):
+            sum += S[j] * L[i + j]
+        num.append(sum)
+
+    answer = max(num)
+    print(f'#{test_case} {answer}')
