52cb69a91a99b62168bb2f46840c8c51f05bc89f
DOEON KIM <vov06144@naver.com>
2022-08-09 09:37:28 +0900

    Commit Test

--- SWEA/1204.py
+++ SWEA/1204.py
@@ -1,4 +1,4 @@
-# 최빈수 구하기
+# 최빈수 구하기!
 
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
@@ -7,7 +7,7 @@ for test_case in range(1, T + 1):
     grades = list(map(int, input().split()))
     max_count = 0
     max_i = 0
-    
+
     ## mistake! 이 부분이 있어야 같은 횟수로 나와도 그 중 큰 값을 선택할 수 있음
     grades_list = sorted(grades)
 
@@ -17,5 +17,5 @@ for test_case in range(1, T + 1):
         if counts >= max_count:
             max_count = counts
             max_i = i
-            
+
     print(f'#{t} {grades_list[max_i]}')
